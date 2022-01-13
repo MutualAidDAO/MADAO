29364a64babb28215a95c7d89d06bea6ac6e2c27
FurrySparkle <97181304+FurrySparkle@users.noreply.github.com>
2022-01-12 22:00:51 -0700

    Fixing menu

--- subscriberform.php
+++ subscriberform.php
@@ -7,7 +7,8 @@ if($_POST["message"]) {
 mail("MutualAidDAO@gmail.com", "New User from Site",
 
 
-$_POST["insert your message here"]. "From: ");
+
+$_POST["insert your message here"]. "From: <input type="email">");
 
 
 }
