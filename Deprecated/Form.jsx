f4feccf36a9c20710254427acb4b95e0a3cd6486
FurrySparkle <97181304+FurrySparkle@users.noreply.github.com>
2022-10-04 13:22:43 -0600

    Web Overhaul V2
    Another big website redesign

---
+++ Deprecated/Form.jsx
@@ -0,0 +1,15 @@
+
+const dotenv = require(['dotenv'])
+
+dotenv.config();
+ 
+
+
+
+const Form = () => {
+  const Send = process.env.SEND;
+  // const data = JSON.stringify()
+  return Send
+};
+console.log(Form());
+
---
+++ Deprecated/FormRender.js
@@ -0,0 +1,7 @@
+import Form from "./Form";
+import DOM from 'react-dom'
+import React from "https://unpkg.com/react@18/umd/react.production.min.js" 
+
+
+const root = DOM.createroot(document.getElementById('Form'));
+root.render(React.createElement(<Form />));
